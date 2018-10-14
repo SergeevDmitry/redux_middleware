f9c9cfb630065669945a5159bb296c81a31d4c86
sergeevdms <sergeevdms@gmail.com>
2018-10-14 17:35:08 +0300

    added API middleware

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,11 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import localStorage, { loadState } from '../middleware/local-storage';
+import apiMiddleware from '../middleware/api';
 
 import reducer from '../reducers';
 
 const initialState = loadState('todos');
-const store = createStore(reducer, initialState, applyMiddleware(thunk, promise, localStorage('todos'), logger));
+const store = createStore(reducer, initialState, applyMiddleware(apiMiddleware('http://localhost:3000/api'), thunk, promise, localStorage('todos'), logger));
 
 export default store;
